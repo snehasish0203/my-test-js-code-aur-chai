e971a6bb58d35414719b1c7880e51cd183b0be6d
snehasish0203 <140537298+snehasish0203@users.noreply.github.com>
2024-06-25 04:19:35 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 763428
+let acoountEmail = "admin@gmail.com"
+var accountPassword ="123456"
+accountCity ="kharagpur"
+
+// accountId=24
+
+accountEmail="sneha@gamil.com"
+accountPassword ="321654"
+accountCity ="noida"
+/****prefer not to use var because of issue in block scope */
+
+console.log(accountId);
+console.table([ accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as a new vertion
+
+//alert(7+7) //we are using nodejs not browser
+
+console.log(4+3) //code readability should be hogh 
+
+console.log("hello suvasree")
+
+let name= "snehasish"
+
+let age="23"
+let isloggedIn=false
+
+// number 2 to power 53
+//bigint
+//string =>""
+//boolean =>true/false
+//null=>standalone value
+//undefined
+//symbol =>unique
+
+
+
+//object
+
+console.log(typeof undefined ); //undefined
+
+console.log(typeof null); //object
