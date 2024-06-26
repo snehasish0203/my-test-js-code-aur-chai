b80f4bf990a615aa27036a6b7f4828e5f004a1f3
snehasish0203 <140537298+snehasish0203@users.noreply.github.com>
2024-06-26 03:38:42 +0000

    convertionOperation

---
+++ 01_basics/03_convertionOperation.js
@@ -0,0 +1,22 @@
+// let score =33
+
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+// let valueInNumber =Number(score)
+// console.log(typeof valueInNumber);
+
+// let isloggedIn=0
+
+// let booleanIsloggedIn=Boolean(isloggedIn)
+
+// console.log(booleanIsloggedIn);
+
+let someNumber =33
+
+let stringNumber= String(someNumber)
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
